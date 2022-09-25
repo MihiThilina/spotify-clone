45b3329c5e5c4681fe1be5169aa177245b2f3375
MihiThilina <thilinabandar67@gmail.com>
2022-09-26 02:55:56 +0530

    install axios

--- src/componets/SearchArea.vue
+++ src/componets/SearchArea.vue
@@ -1,24 +1,29 @@
-
-<script setup>
+<script>
+export default {
+  methods: {
+    keyPressed(event){
+        // console.log("presssd")
+        var key = event.target.value;
+        console.log(key)
+    }
+    
+  },
+};
 </script>
-
 
 <template>
   <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label"
-      >Search Music</label
-    >
+    <label for="staticEmail" class="col-sm-2 col-form-label">Search Music</label>
     <div class="col-sm-10">
       <input
         type="text"
         class="form-control"
         id="staticEmail"
         placeholder="Type here"
+        @input="keyPressed"
       />
     </div>
   </div>
 </template>
 
-
-<style>
-</style>
+<style></style>
