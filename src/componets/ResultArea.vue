8a18b8db5b58a496e39fd7c5bdc4d6b236d3cd20
MihiThilina <thilinabandar67@gmail.com>
2022-09-27 02:42:12 +0530

    Updated Result Section

--- src/componets/ResultArea.vue
+++ src/componets/ResultArea.vue
@@ -5,8 +5,28 @@
 <template>
   <div
     class="main"
-    style="position: absolute; width: 83vw; left: 258px; height: 100vh; top: 0"
+    style="
+      position: absolute;
+      width: 83vw;
+      left: 258px;
+      height: 100vh;
+      top: 0;
+    "
   >
+    <div class="avatar">
+      <va-avatar
+        style="position: absolute; left: 90px; top: 5px"
+        src="https://media-exp1.licdn.com/dms/image/C4D03AQFI3NGHwjy_hA/profile-displayphoto-shrink_800_800/0/1635346771654?e=1669852800&v=beta&t=KaglBbGpZ-Vw7ChvwP-BJryVFUw4DEYij6YcmFuGsVM"
+      />
+      <va-dropdown style="position: relative; left: 150px; top: 10px;">
+        <template #anchor>
+          <va-button style="width:9vw; background-color:#323231; color:white;"> Thilina </va-button>
+        </template>
+
+        <va-dropdown-content> ! </va-dropdown-content>
+      </va-dropdown>
+    </div>
+
     <h2
       style="
         position: absolute;
@@ -19,104 +39,463 @@
       Recent Played
     </h2>
 
+    <h2
+      style="
+        position: absolute;
+        right: 60px;
+        font-size: 15px;
+        color: white;
+        top: 107px;
+      "
+    >
+      SEE ALL
+    </h2>
+
     <div class="resultCards">
-
       <div class="cards">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
 
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2
+      style="
+        position: absolute;
+        left: 40px;
+        font-size: 20px;
+        color: white;
+        top: 370px;
+      "
+    >
+      Your heavy rotalion
+    </h2>
 
-      <div class="cards">
+    <h2
+      style="
+        position: absolute;
+        right: 60px;
+        font-size: 15px;
+        color: white;
+        top: 373px;
+      "
+    >
+      SEE ALL
+    </h2>
+
+    <div class="resultCards2">
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
-      <div class="cards">
+
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
-      <div class="cards">
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
-      <div class="cards">
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
-      <div class="cards">
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
       </div>
-      <div class="cards">
+      <div class="cards2">
         <div class="row">
           <div class="flex md6 lg12">
-            <va-card style="height: 25vh;">
+            <va-card style="height: 25vh">
               <va-image
                 src="https://picsum.photos/400/200"
                 style="height: 17vh"
               />
-              <va-card-title style="position: relative; top:-10px; font-weight:900; font-size: 14px;">Title</va-card-title>
-              <va-card-title style="position: relative; top:-25px; font-weight:100; font-size: 10px;">Kasun Kalhara</va-card-title>
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
+            </va-card>
+          </div>
+        </div>
+      </div>
+      <div class="cards2">
+        <div class="row">
+          <div class="flex md6 lg12">
+            <va-card style="height: 25vh">
+              <va-image
+                src="https://picsum.photos/400/200"
+                style="height: 17vh"
+              />
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -10px;
+                  font-weight: 900;
+                  font-size: 14px;
+                "
+                >Title</va-card-title
+              >
+              <va-card-title
+                style="
+                  position: relative;
+                  top: -25px;
+                  font-weight: 100;
+                  font-size: 10px;
+                "
+                >Kasun Kalhara</va-card-title
+              >
             </va-card>
           </div>
         </div>
